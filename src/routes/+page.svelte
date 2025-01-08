a9a35a27722239b99b5e158a1aec99322d17d76d
khanh.tran <khanh.tran@lgcns.com>
2025-01-08 15:01:55 +0700

    🔧 Fix home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,23 +8,25 @@
 <main class="container mx-auto">
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 		{#each Object.keys(layout) as name}
-			<div class="card w-full bg-base-100 shadow-xl">
-				<figure>
-					<img src="/images/placeholder.png" alt="Shoes" />
-				</figure>
-				<div class="card-body">
-					<h2 class="card-title">
-						{name}
-						<div class="badge badge-secondary">NEW</div>
-					</h2>
-					<ul class="menu bg-base-100 p-4">
-						{#each layout[name] as article}
-							<li>
-								<a href={"/blog/" + article.id}>{article.title}</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+			<div>
+				<h2 class="text-2xl font-bold">{name}</h2>
+				<ul>
+					{#each layout[name] as article}
+						<li>
+							<div class="card w-96 bg-base-100 shadow-xl">
+								<figure>
+									<img src={article.coverImage} alt="Cover Image" />
+								</figure>
+								<div class="card-body">
+									<h2 class="card-title">
+										{article.title}
+									</h2>
+									<div class="card-actions justify-end"></div>
+								</div>
+							</div>
+						</li>
+					{/each}
+				</ul>
 			</div>
 		{/each}
 	</div>
